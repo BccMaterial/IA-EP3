9a52da5be31f878d072521956ca71c37d924db49
thiagopls1 <thiagopls1@hotmail.com>
2025-04-14 23:45:55 -0300

    Update scripts

--- setup.ps1
+++ setup.ps1
@@ -5,6 +5,3 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 python -m venv .venv
 .venv/Scripts/Activate.ps1
 pip install -r requirements.txt
-
-# Execução
-python main.py
--- setup.sh
+++ setup.sh
@@ -2,6 +2,3 @@
 python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-
-# Execução
-python main.py
